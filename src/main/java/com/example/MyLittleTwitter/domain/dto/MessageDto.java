67d2273feb6b575047d993bcdcd8cb0c1b928b6e
Marko Maend <marko.maend@gmail.com>
2020-11-13 02:14:53 +1100

    Add likes, HQL : JPQL

---
+++ src/main/java/com/example/MyLittleTwitter/domain/dto/MessageDto.java
@@ -0,0 +1,68 @@
+package com.example.MyLittleTwitter.domain.dto;
+
+import com.example.MyLittleTwitter.domain.Message;
+import com.example.MyLittleTwitter.domain.User;
+import com.example.MyLittleTwitter.domain.util.MessageHelper;
+
+
+public class MessageDto {
+    private Integer id;
+    private String text;
+    private String tag;
+    private User author;
+    private String filename;
+    private Long likes;
+    private Boolean meLiked;
+
+    public MessageDto(Message message, Long likes, Boolean meLiked) {
+        this.id = message.getId();
+        this.text = message.getText();
+        this.tag = message.getTag();
+        this.author = message.getAuthor();
+        this.filename = message.getFilename();
+        this.likes = likes;
+        this.meLiked = meLiked;
+    }
+
+    public String getAuthorName() {
+        return MessageHelper.getAuthorName(author);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public Long getLikes() {
+        return likes;
+    }
+
+    public Boolean getMeLiked() {
+        return meLiked;
+    }
+
+    @Override
+    public String toString() {
+        return "MessageDto{" +
+                "id=" + id +
+                ", author=" + author +
+                ", likes=" + likes +
+                ", meLiked=" + meLiked +
+                '}';
+    }
+}
--- src/main/java/com/example/MyLittleTwitter/repos/MessageRepo.java
+++ src/main/java/com/example/MyLittleTwitter/repos/MessageRepo.java
@@ -1,14 +1,42 @@
 package com.example.MyLittleTwitter.repos;
 
 import com.example.MyLittleTwitter.domain.Message;
+import com.example.MyLittleTwitter.domain.User;
+import com.example.MyLittleTwitter.domain.dto.MessageDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
-public interface MessageRepo extends CrudRepository<Message, Integer> {
+public interface MessageRepo extends CrudRepository<Message, Long> {
 
-    Page<Message> findAll(Pageable pageable);
+    @Query("select new com.example.MyLittleTwitter.domain.dto.MessageDto(" +
+            "   m, " +
+            "   count(ml), " +
+            "   sum(case when ml = :user  then 1 else 0 end)> 0" +
+            ") " +
+            "from Message m left join m.likes ml " +
+            "group by m")
+    Page<MessageDto> findAll(Pageable pageable, @Param("user") User user);
 
-    Page<Message> findByTag(String tag, Pageable pageable);
+    @Query("select new com.example.MyLittleTwitter.domain.dto.MessageDto(" +
+            "   m, " +
+            "   count(ml), " +
+            "   sum(case when ml = :user  then 1 else 0 end)> 0" +
+            ") " +
+            "from Message m left join m.likes ml " +
+            "where m.tag = :tag " +
+            "group by m")
+    Page<MessageDto> findByTag(@Param("tag") String tag, Pageable pageable, @Param("user") User user);
 
+    @Query("select new com.example.MyLittleTwitter.domain.dto.MessageDto(" +
+            "   m, " +
+            "   count(ml), " +
+            "   sum(case when ml = :user  then 1 else 0 end)> 0" +
+            ") " +
+            "from Message m left join m.likes ml " +
+            "where m.author = :author " +
+            "group by m")
+    Page<MessageDto> findByUser(Pageable pageable, @Param("author") User author, @Param("user") User user);
 }
