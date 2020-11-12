f071849836b4e1f1ac95577064f8e691701516a7
Marko Maend <marko.maend@gmail.com>
2020-11-12 22:27:06 +1100

    Add pagination

--- src/main/java/com/example/MyLittleTwitter/repos/MessageRepo.java
+++ src/main/java/com/example/MyLittleTwitter/repos/MessageRepo.java
@@ -1,12 +1,14 @@
 package com.example.MyLittleTwitter.repos;
 
 import com.example.MyLittleTwitter.domain.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 public interface MessageRepo extends CrudRepository<Message, Integer> {
 
-    List<Message> findByTag(String tag);
+    Page<Message> findAll(Pageable pageable);
+
+    Page<Message> findByTag(String tag, Pageable pageable);
 
 }
